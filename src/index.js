9b3aaa3e7b642dc451bd99d40059e9a685e6e932
Jerromy <jerromy_lee@nox.com.tw>
2022-09-07 15:58:38 +0800

    Add Clean Dist

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import HelloWorldButton from './components/hellow-world-button/hello-world-button';
+import HelloWorldButton from './components/hellow-world-button/hello-world-button.js';
+import Heading from "./components/heading/heading.js";
 
 
+const head = new Heading();
+head.render();
 const hBtn = new HelloWorldButton();
 hBtn.render();
+hBtn.render();
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 
 const path = require( 'path' );
+const TerserPlugin = require( 'terser-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+//const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
 
 /*
 
@@ -9,6 +12,7 @@ asset/inline
 asset/
 asset/source
 
+css-loader => MiniCssExtractPlugin.loader
 
 */
 
@@ -16,9 +20,13 @@ module.exports = {
 
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[contenthash].js',
         path: path.resolve( __dirname, './dist' ),
-        publicPath: 'dist/'
+        publicPath: 'dist/',
+        clean: {
+            dry: true,
+            keep: /\.css/
+        },
     },
     mode: 'none',
     module: {
@@ -39,13 +47,13 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader', 'css-loader'
+                    MiniCssExtractPlugin.loader, 'css-loader'
                 ]
             },
             {
                 test: /\.scss$/,
                 use: [
-                    'style-loader', 'css-loader', 'sass-loader'
+                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
                 ]
             },
             {
@@ -60,7 +68,19 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new TerserPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css',
+        }),
+        // new CleanWebpackPlugin({
+        //     cleanOnceBeforeBuildPatterns: [
+        //         '**/*',
+        //         path.join(process.cwd(), 'build/**/*' )
+        //     ]
+        // }),
+    ]
 
 
 };
